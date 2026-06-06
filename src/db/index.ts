956658486a67e01244f53db373e4881d0f072bfa
sonu-shivcharan <sonushivcharan@outlook.com>
2026-06-06 15:40:21 +0530

    feat: create schema index file to export all schemas and init drizzle with schema

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,9 @@
 import { Pool } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
-
+import * as schema from "./schema";
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   max: 10,
 });
 
-export const db = drizzle(pool);
+export const db = drizzle(pool, { schema });
---
+++ src/db/schema/index.ts
@@ -0,0 +1,3 @@
+export * from "./users";
+export * from "./stores";
+export * from "./ratings";
