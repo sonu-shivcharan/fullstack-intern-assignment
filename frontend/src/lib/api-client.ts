01540ae1a2497fe85b5b91b014c5c64e16154910
sonu-shivcharan <sonushivcharan@outlook.com>
2026-06-08 13:13:59 +0530

    intercep api response to show server error msg

--- frontend/src/lib/api-client.ts
+++ frontend/src/lib/api-client.ts
@@ -1,4 +1,5 @@
-import axios from "axios";
+import type { ApiErrorResponse } from "@/types/api";
+import axios, { type AxiosError } from "axios";
 
 export const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
@@ -8,3 +9,16 @@ export const apiClient = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error: AxiosError) => {
+    const serverErr = error.response;
+    const data = serverErr?.data as ApiErrorResponse | undefined;
+
+    if (data?.message) {
+      error.message = data.message;
+    }
+    return Promise.reject(error);
+  }
+);
